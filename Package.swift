86e450c884d4ab151d602b5d6e513a89947ebd22
Daniel Tull <dt@danieltull.co.uk>
2020-02-28 09:50:07 +0000

    Use swift-argument-parser to create a parsing tool

--- Package.swift
+++ Package.swift
@@ -8,9 +8,15 @@ let package = Package(
         .library(name: "RomanNumerals", targets: ["RomanNumerals"]),
         .executable(name: "roman", targets: ["RomanNumeralsTool"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
+    ],
     targets: [
         .target(name: "RomanNumerals"),
         .testTarget(name: "RomanNumeralsTests", dependencies: ["RomanNumerals"]),
-        .target(name: "RomanNumeralsTool", dependencies: ["RomanNumerals"])
+        .target(name: "RomanNumeralsTool", dependencies: [
+            "RomanNumerals",
+            "ArgumentParser"
+        ])
     ]
 )
--- Sources/RomanNumeralsTool/main.swift
+++ Sources/RomanNumeralsTool/main.swift
@@ -1,13 +1,37 @@
 
-import Foundation
+import ArgumentParser
 import RomanNumerals
 
-let arguments = CommandLine.arguments
+struct Roman: ParsableCommand {
 
-do {
-    let expression = try Expression(arguments: Array(arguments.dropFirst()))
-    let result = expression.calculate()
-    print(result)
-} catch {
-    print(error)
+    static let configuration = CommandConfiguration(
+        abstract: "Roman numerals utility.",
+        subcommands: [To.self, From.self])
 }
+
+extension Roman {
+
+    struct From: ParsableCommand {
+
+        @Argument(help: "The roman numerals to convert to numbers.")
+        var numerals: String
+
+        func run() throws {
+            let numerals = try RomanNumeral(self.numerals)
+            print(Int(numerals))
+        }
+    }
+
+    struct To: ParsableCommand {
+
+        @Argument(help: "The value to convert to roman numerals.")
+        var value: Int
+
+        func run() {
+            let numerals = RomanNumeral(value)
+            print(numerals.description)
+        }
+    }
+}
+
+Roman.main()
